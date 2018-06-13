3b0114505919af34baef7d2aee695aa83cc51ff4
Rebecca Govert <becci.govert@gmail.com>
2018-06-13 15:56:25 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,6 @@
 def oxford_comma(array)
   return array[0] if array.length == 1 
-  return array [0.. -2].join(", ") + " and " + array[-1] if array.length > 1
+  return array[0..-1].join(" and ") if array.length == 2
+  return array [0.. -2].join(", ") + ", and " + array[-1] if array.length > 1
 end
 #[list[0...-1].join(", "), list.last].join(", and ")
