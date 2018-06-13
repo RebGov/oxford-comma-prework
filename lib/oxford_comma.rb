2a8d939a955f83c6c2e634dc8f713e2b837fc526
Rebecca Govert <becci.govert@gmail.com>
2018-06-13 15:55:07 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,5 @@
 def oxford_comma(array)
   return array[0] if array.length == 1 
-  return array [0.. -2].join(", ") + " and " + array[-1] if array. length > 1
+  return array [0.. -2].join(", ") + " and " + array[-1] if array.length > 1
 end
 #[list[0...-1].join(", "), list.last].join(", and ")
